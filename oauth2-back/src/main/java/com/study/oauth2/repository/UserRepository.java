2846628b15112f3255e2103495c4aaad9621ee66
jw0302 <wkdwlsdnjs41@naver.com>
2023-05-10 17:51:13 +0900

    계정연동 수업

---
+++ oauth2-back/src/main/java/com/study/oauth2/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.study.oauth2.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.study.oauth2.entity.Authority;
+import com.study.oauth2.entity.User;
+
+@Mapper
+public interface UserRepository {
+
+//	public int addAuthorities(List<Authority> authorities);
+	// User 이메일 조회
+	public User findUserByEmail(String email);
+	// 회원가입
+	public int saveUser(User user);
+	public int saveAuthority(Authority authority);
+}
